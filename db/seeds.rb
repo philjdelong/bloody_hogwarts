23d56adbe3812afdcd4c6632a6baa7e32460e60e
Phil DeLong <philjdelong@gmail.com>
2019-12-06 09:34:12 -0700

    Complete user story 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+casseopia_black = Student.create(
+    name: "Casseopia Black",
+    age: 14,
+    house: "Syltherin"
+  )
