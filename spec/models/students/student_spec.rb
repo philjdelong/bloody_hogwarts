8db650337d632c4ea93426c489644a6f2a858fae
Phil DeLong <philjdelong@gmail.com>
2019-12-06 12:00:09 -0700

    Write test for average method

--- spec/models/students/student_spec.rb
+++ spec/models/students/student_spec.rb
@@ -13,4 +13,26 @@ RSpec.describe Student, type: :model do
     it {should have_many(:courses).through(:student_courses)}
   end
 
+  describe 'class methods' do
+    it ".average_age" do
+      students = Student.all
+
+      h_potter = Student.create(
+          name: "Harry Potter",
+          age: 12,
+          house: "Gryffindor")
+
+      c_black = Student.create(
+          name: "Casseopia Black",
+          age: 14,
+          house: "Syltherin")
+
+      n_longbottom = Student.create(
+          name: "Nevil Longbottom",
+          age: 13,
+          house: "Gryffindor")
+
+      expect(students.average_age).to eq(13)
+    end
+  end
 end
