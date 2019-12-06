9ef971b22fe6da4ca6dd1a7908a10d794a8c45db
Phil DeLong <philjdelong@gmail.com>
2019-12-06 10:39:54 -0700

    Start model test for courses total students method. Incomplete

---
+++ spec/models/courses/add_students_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "methods", type: :method do
+  before :each do
+    @wizardry = Courses.create("Wizardry")
+  end
+    it "a course can tell me how many students are enrolled" do
+    
+    end
+  end
+end
