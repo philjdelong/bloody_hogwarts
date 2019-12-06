36ae91e3955c81e9ab351478dcdcc1fee7f395ef
Phil DeLong <philjdelong@gmail.com>
2019-12-06 09:55:06 -0700

    Add show to students controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,4 +3,9 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show
+    @student = Student.find(params[:id])
+    @courses = @student.courses
+  end
 end
